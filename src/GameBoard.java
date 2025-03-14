9c9876965e5501dc722bc46558124f7a406ad998
Danny <megaslashgt123@gmail.com>
2025-03-14 13:40:00 -0400

    Board setup completed

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,34 @@
 public class GameBoard {
+    private Space[][] gameBoard;
+    private String player1;
+    private String player2;
+
+    public GameBoard(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        gameBoard = new Space[6][7];
+    }
+
+    public void start() {
+        setUpBoard();
+        printBoard();
+    }
+
+    public void setUpBoard() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[0].length; j++) {
+                gameBoard[i][j] = new Space("＿");
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[0].length; j++) {
+                System.out.print(gameBoard[i][j].getSymbol() + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,2 +1,6 @@
 public class GameRunner {
+    public static void main(String args[]) {
+        GameBoard game = new GameBoard("Player1", "Player2");
+        game.start();
+    }
 }
